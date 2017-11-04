2509ab1592b56614bb5af707c185f98da74e6694
Skyler Svendsen <skylersvendsen@gmail.com>
2017-11-03 23:35:22 -0500

    first commit

---
+++ HwSProject37/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  HwSProject37
+//
+//  Created by Skyler Svendsen on 11/3/17.
+//  Copyright © 2017 Skyler Svendsen. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
